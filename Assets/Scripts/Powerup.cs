f26b56950c60b3850cc186cc953093b26ead78a7
REISU-LEOPARD\reisu <reisuj@gmail.com>
2021-04-05 20:11:45 -0400

    Setup basic TripleShot behaviour

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * _speed * Time.deltaTime);
+        if (transform.position.y < -3.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player collected powerup");
+            Destroy(this.gameObject);
+        }
+    }
+}
